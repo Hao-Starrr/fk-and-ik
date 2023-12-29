b3531e57af866fb5a571beb66b6add31307ac7fc
Wang Wenhao <63582163+Hao-Starrr@users.noreply.github.com>
2023-12-28 20:11:54 -0500

    make

---
+++ build/CMakeFiles/RobotArm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RobotArm.dir/main.cpp.obj"
+  "CMakeFiles/RobotArm.dir/main.cpp.obj.d"
+  "RobotArm.exe"
+  "RobotArm.exe.manifest"
+  "RobotArm.pdb"
+  "libRobotArm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RobotArm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
